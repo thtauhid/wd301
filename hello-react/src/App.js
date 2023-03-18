840ac68799bece11e8d712dc4d21a16c1443de73
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-03-19 00:33:28 +0530

    New task button updated

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -22,6 +22,23 @@ function App() {
             dueDate='22nd March'
             assigneeName='Rohit M'
           />
+          <button className='flex bg-stone-400 p-3 m-2 text-stone-900 text-xl font-bold items-center'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              class='w-6 h-6 mr-2'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M12 4.5v15m7.5-7.5h-15'
+              />
+            </svg>
+            New Task
+          </button>
         </div>
         <div className='basis-1/2 flex flex-col border-2 border-stone-400 rounded p-5 ml-5'>
           <h1 className='font-bold text-2xl pb-3 text-center text-stone-500'>
