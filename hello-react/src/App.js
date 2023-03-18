dfb0b93379c7402edfb48c4a7fc37271b9eaa5ef
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-03-19 00:23:22 +0530

    Logic and code updated for lvl1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -10,13 +10,29 @@ function App() {
       <div className='flex flex-row justify-around mt-5'>
         <div className='basis-1/2 flex flex-col border-2 border-stone-300 rounded p-5 mr-5'>
           <h1 className='font-bold text-2xl pb-3'>Pending</h1>
-          <TaskCard title='x' />
-          <TaskCard title='x' />
+          <TaskCard
+            title='Build the website with static content'
+            dueDate='10th April'
+            assigneeName='Rohit S'
+          />
+          <TaskCard
+            title='Add a blog'
+            dueDate='22nd March'
+            assigneeName='Rohit M'
+          />
         </div>
         <div className='basis-1/2 flex flex-col border-2 border-stone-300 rounded p-5 ml-5'>
           <h1 className='font-bold text-2xl py-3'>Done</h1>
-          <TaskCard title='x' />
-          <TaskCard title='x' />
+          <TaskCard
+            title='Design a mockup'
+            completedAtDate='10th April'
+            assigneeName='Rohit M'
+          />
+          <TaskCard
+            title='Get the approval from principal'
+            completedAtDate='20th April'
+            assigneeName='Ajay S'
+          />
         </div>
       </div>
     </div>
--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -2,11 +2,19 @@ import "./TaskCard.css";
 
 const TaskCard = (props) => {
   console.log(props);
+
   return (
     <div className='TaskItem '>
       <h2 className='text-xl font-bold'>{props.title}</h2>
-      <p>Completed on: due date...</p>
-      <p>Assignee: name...</p>
+      {
+        // If the task is completed, show the completed date else show the due date
+        props.completedAtDate ? (
+          <p>Completed on: {props.completedAtDate}</p>
+        ) : (
+          <p>Due on: {props.dueDate}</p>
+        )
+      }
+      <p>Assignee: {props.assigneeName}</p>
     </div>
   );
 };
